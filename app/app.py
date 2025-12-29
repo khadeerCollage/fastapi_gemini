7e564ebb43950a0c3899342fcbf8a11cbc92f678
khadeeCollege <o210855@rguktong.ac.in>
2025-12-29 16:07:37 +0530

    all auth and prompt set up'ed

--- app/app.py
+++ app/app.py
@@ -1,9 +1,28 @@
 import os 
-from fastapi import FastAPI
+from fastapi import FastAPI , Depends
 from pydantic import BaseModel
+from src.ai.gemini import Gemini
+from src.auth.dependencies import get_user_identifier
+from src.auth.throttling import apply_rate_limit
+
 
 
 app = FastAPI()
+
+def load_system_prompt():
+    try:
+        with open("src/prompts/system_prompt.md", "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        return None
+
+system_prompt = load_system_prompt()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY environment variable not set")
+
+ai_platform = Gemini(api_key=gemini_api_key, system_prompt=system_prompt)
 
 
 class ChatRequest(BaseModel):
@@ -13,12 +32,14 @@ class ChatResponse(BaseModel):
     response: str
     
 
+@app.post("/chat", response_model=ChatResponse)
+async def chat_endpoint(request: ChatRequest, user_id:str = Depends(get_user_identifier)):
+    apply_rate_limit(user_id)
+    
+    response_text = ai_platform.chat(request.prompt)
+    return ChatResponse(response=response_text)
+
+
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the Chat API!"}
-
-@app.post("/chat", response_model=ChatResponse)
-async def chat_endpoint(request: ChatRequest):
-    reponse_text = "..."
-    return ChatResponse(response=reponse_text)
-
---
+++ src/ai/gemini.py
@@ -0,0 +1,21 @@
+from src.ai.base import AIPlatform
+import google.generativeai as genai
+import os 
+
+
+
+
+class Gemini(AIPlatform):
+    def __init__(self,api_key:str,system_prompt:str =None):
+        self.api_key = api_key
+        self.system_prompt = system_prompt
+        genai.configure(api_key=self.api_key)
+        self.model = genai.GenerativeModel("gemini-2.5-flash-preview-05-20")
+        
+    def chat(self,prompts:str) -> str:
+        if self.system_prompt:
+            prompt = f"{self.system_prompt}\n{prompts}"
+        response = self.model.generate_content(prompt)
+        return response.text
+    
+        
